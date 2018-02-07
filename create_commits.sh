319aadb8d17180aa914a6a99aba591cb8e1bac63
Jacek Ławniczak <jacekelgda@gmail.com>
2018-02-07 11:35:23 +0100

    update number 1

--- create_commits.sh
+++ create_commits.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 counter=1
-while [ $counter -le 10 ]
+while [ $counter -le 500 ]
 do
 	echo $counter >> change.txt
 	((counter % 5 == 0)) && echo "API_KEY=$(echo -n $counter | shasum -a 1 | awk '{print $1}')" > config.env
